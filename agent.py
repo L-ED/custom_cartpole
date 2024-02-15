153a43335b9e6d9e26683c8bf2b068800a061ba7
L-ED <lyginegor00@gmail.com>
2024-02-15 19:47:09 +0300

    added buffer, started agent

--- agent.py
+++ agent.py
@@ -1,4 +1,41 @@
 import torch
 from torch import nn
 
-class MLPPolicy()
+def linear(inp_shape, out_shape, act, use_bn):
+    layer = [
+        torch.nn.Linear(inp_shape, out_shape),
+        act()
+    ]
+    if use_bn:
+        layer.insert(1, torch.nn.BatchNorm1d(out_shape))
+    return layer
+
+
+class MLP(torch.nn.Module):
+
+    def __init__(
+            self, 
+            inp_shape, 
+            out_shape, 
+            hidden_shape=64, 
+            hidden_num=1, 
+            act= torch.nn.ReLU, 
+            use_bn = False
+        ):
+
+        self.stem = linear(inp_shape, hidden_shape, act, use_bn)
+        self.stem.extend(
+            linear(hidden_shape, hidden_shape, act, use_bn)*hidden_num
+        )
+        self.stem.append(linear(hidden_shape, out_shape, act, use_bn))
+        self.stem = torch.nn.Sequential(self.stem)
+
+    def forward(self, x):
+        return self.stem(x)
+
+                
+class ActorCritic(torch.nn.Module):
+
+    def __init__(self, obs_space, act_space):
+
+        self.actor = MLP(obs_space, act_space)
--- ppo.py
+++ ppo.py
@@ -1,9 +1,75 @@
 import torch
 from torch.optim import Adam
+import numpy as np
+import scipy
 
 class Buffer:
     # folder for collecting stat-acttion pairs
-    pass
+    def __init__(self, gamma, lambd) -> None:
+        self.gamma = gamma
+        self.lambd = lambd
+        self.sts = np.zeros(0)
+        self.act = np.zeros(0)
+        self.rew = np.zeros(0)
+        self.val = np.zeros(0)
+        self.log_probs = np.zeros(0)
+
+        self.traj_lenght= 0
+
+        self.rtg = np.zeros(0) #np.array([])
+        self.adv = np.zeros(0) #np.array([])
+
+
+    def collect(self, state, action, value, reward, log_prob):
+
+        self.sts = np.append(self.sts, state)
+        self.act = np.append(self.act, action)
+        self.rew = np.append(self.rew, reward)
+        self.val = np.append(self.val, value)
+        self.log_probs = np.append(self.log_probs, log_prob)
+    
+        self.traj_lenght +=1
+
+    
+    def end_traj(self, last_val):
+
+        rew = self.rew[-self.traj_lenght:]
+        val = self.val[-self.traj_lenght:]
+
+        rew = np.append(rew, last_val)
+        val = np.append(val, last_val)
+
+        delta = rew[:-1] + self.gamma*val[1:] - val[:-1]
+
+        adv = self.discounted_sum(delta, self.gamma*self.lambd)
+        rtg = self.discounted_sum(delta, self.gamma)
+
+        self.adv = np.append(self.adv, adv)
+        self.rtg = np.append(self.rtg, rtg)
+
+
+    def discounted_sum(sequence, coef):
+        return scipy.signal.lfilter(
+            [1], [1, float(-coef)], sequence[::-1], axis=0)[::-1]
+
+
+    def get(self):
+        self.normalize_advantage()
+        data = [
+            self.sts, 
+            self.act, 
+            self.rtg,
+            self.adv, 
+            self.log_prob
+        ]
+        
+        return [torch.from_numpy(v) for v in data]
+
+
+    def normalize_advantage(self):
+        mean, std = self.adv.mean(), self.adv.std()
+        self.adv = (self.adv - mean)/std
+
 
 
 class PPO:
@@ -31,9 +97,10 @@ class PPO:
             traj_num = 0
             reward_collector = 0
             for iteration in self.epoch_iters:
-                action, val = self.agent(state)
-                state,rew,term,trunc,_ = self.env.step()
-                self.buffer.collect(state, action, val, rew)
+                action, val, log_prob = self.agent(state)
+                next_state,rew,term,trunc,_ = self.env.step()
+                self.buffer.collect(state, action, val, rew, log_prob)
+                state = next_state
 
                 last_iter = iteration == self.epoch_iters-1
                 reward_collector += rew
@@ -72,11 +139,11 @@ class PPO:
             self.policy_optim.zero_grad()
 
         
-    def policy_loss(self, data):
+    def loss(self, data):
         
-        obs, act, adv, rew, log_prob_old = data 
+        states, act, rtg, adv, log_prob_old = data 
 
-        pi, log_prob = self.agent.policy(obs, act)
+        pi, log_prob = self.agent.policy(states, act)
         ratio = torch.exp(log_prob - log_prob_old)
         ratio_cliped = torch.clamp(
             ratio, 1-self.clip_eps, 1+self.clip_eps)
@@ -85,7 +152,7 @@ class PPO:
         if self.agent.shared:
             p_loss += pi.entropy()
 
-        v_loss = ((self.agent.value(obs) - rew)**2).mean()
+        v_loss = ((self.agent.value(states) - rtg)**2).mean()
 
         return p_loss, v_loss
     
